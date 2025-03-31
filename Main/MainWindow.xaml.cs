97507423d3926b1155fc6362b6c08c585da917db
di-cherry <diano4ka01082006@gmail.com>
2025-03-31 21:35:58 +0300

    Add files via upload

---
+++ Main/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PR6._2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Auth(log.Text, pass.Password);
+        }
+        public bool Auth(string login, string password)
+        {
+            if (string.IsNullOrEmpty(login) || string.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("Введите логин и пароль!");
+                return false;
+            }
+
+            using (var db = new Entities())
+            {
+                var user = db.Users.AsNoTracking().FirstOrDefault(u => u.Login == login && u.Password == password);
+
+                if (user == null)
+                {
+                    MessageBox.Show("Пользователь с такими данными не найден!");
+                    return false;
+                }
+                MessageBox.Show("Пириует");
+                log.Clear();
+                pass.Clear();
+                return true;
+            }
+        }
+
+        private void BtnRegister_Click(object sender, RoutedEventArgs e)
+        {
+            RegPage regpag = new RegPage();
+            this.Content = regpag;
+        }
+    }
+
+}
---
+++ Main/RegPage.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PR6._2
+{
+    /// <summary>
+    /// Логика взаимодействия для RegPage.xaml
+    /// </summary>
+    public partial class RegPage : Page
+    {
+        public RegPage()
+        {
+            InitializeComponent();
+        }
+        private void BtnRegister_Click(object sender, RoutedEventArgs e)
+        {
+            Reg(TextBoxFIO.Text, TextBoxLogin.Text, PasswordBox.Password, CmbRole.Text);
+        }
+
+        public bool Reg(string fio, string login, string password, string role)
+        {
+            if (string.IsNullOrEmpty(fio) || string.IsNullOrEmpty(login) || string.IsNullOrEmpty(password) || string.IsNullOrEmpty(role))
+            {
+                MessageBox.Show("Заполните все обязательные поля!", "Ошибка");
+                return false;
+            }
+
+            using (var db = new Entities())
+            {
+                if (db.Users.Any(u => u.FIO == fio && u.Login == login && u.Password == password && u.Role == role))
+                {
+                    MessageBox.Show("Пользователь с таким логином уже существует!", "Ошибка");
+                    return false;
+                }
+
+                var newUser = new Users
+                {
+                    FIO = fio,
+                    Login = login,
+                    Password = password,
+                    Role = role,
+                };
+
+                db.Users.Add(newUser);
+                db.SaveChanges();
+
+                MessageBox.Show("Регистрация прошла успешно!", "Успех");
+                return true;
+
+            }
+        }
+
+        private void BtnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.GoBack();
+
+            TextBoxFIO.Clear();
+            TextBoxLogin.Clear();
+            PasswordBox.Clear();
+            CmbRole.SelectedIndex = 0;
+        }
+
+
+    }
+}
